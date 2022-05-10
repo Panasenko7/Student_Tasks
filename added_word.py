cff72bfeefd8adc7b85aeb4cc25a58444d64da52
Andrey V <radioactiveguide@ukr.net>
2022-05-10 12:16:43 +0300

    v.2.0

--- added_word.py
+++ added_word.py
@@ -1,10 +1,11 @@
-input_list = ["javelin", "bayraktar", "huilo", "banana"]
+input_list = ["javelin", "bayraktar", "hello", "banana"]
 
 
-def qqq(input_list_):
-    return ["Hui" + string[2:] + f'-{len(string)}'
+def word(input_list_):
+    return ["PHY" + string[2:] + f' {len(string)}'
             if string[1] in ['a', 'e', 'o', 'i', 'u', 'y', 'j']
-            else "Hui" + string[1:] + len(string) for string in input_list_]
+            else "PHY" + string[1:] + len(string) for string in input_list_]
 
 
-print(qqq(input_list))
+print(word(input_list))
+
--- to_recognize_anagram.py
+++ to_recognize_anagram.py
@@ -1,5 +1,13 @@
-#A="Buckethead "
+#A="Buckethead"
 #B="DeathCubeK"
-def verify_anagrams(A,B):
-    return "".join(sorted(A.lower())).strip() == "".join(sorted(B.lower())).strip()
-print (verify_anagrams("Buckethead ","DeathCubeK"))    #True
+#def verify_anagrams(A,B):
+    #return "".join(sorted(A.lower())).strip() == "".join(sorted(B.lower())).strip()
+#print (verify_anagrams("Buckethead ","DeathCubeK"))    #True
+
+is_anagram = lambda x1, x2: sorted(x1.lower()) == sorted(x2.lower())
+
+
+#Results
+print(is_anagram("Buckethead", "DeathCubeK"))
+print(is_anagram("elvise", "livees"))
+print(is_anagram("elvis", "dead"))
