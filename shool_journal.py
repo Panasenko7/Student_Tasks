9c2a681e0ae91a7dd343cb45da7048191e6286ee
Andrey V <radioactiveguide@ukr.net>
2022-05-12 15:51:58 +0300

    Not Done compeletely. Need to fix students calc.

---
+++ shool_journal.py
@@ -0,0 +1,21 @@
+input_total_amount = input('Enter total amount of students:')
+input_total_amount = int(input_total_amount)  # asking/entering total amount of people
+students_list_ = []  # creating an empty list to add all of them
+
+
+for student in range(0, input_total_amount):
+
+
+    for student in range(1, input_total_amount):
+
+        input_student = input('Enter the' + f' {len(students_list_)}st ' + 'student:')
+        input_student = str(input_student)  # asking/entering each current student
+        students_list_.append(str(input_student))
+
+
+print('You have entered all of them. Here they are:')
+print(students_list_)
+
+
+
+
