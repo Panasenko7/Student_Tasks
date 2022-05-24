75655d5270386d7ad6fae6878aa163299ee636ee
Andrey V <radioactiveguide@ukr.net>
2022-05-24 20:30:37 +0300

    with country

--- school_journal/shool_journal.py
+++ school_journal/shool_journal.py
@@ -5,28 +5,52 @@ GENDER_DETAILS_URL = 'https://api.genderize.io?name='
 NATION_URL = 'https://api.nationalize.io?name='
 
 
+def divide_lists_by_ten(input_list):
+    result_list = [[]]
+
+    for element in input_list:
+        if len(result_list[-1]) <= 10:
+            result_list[-1].append(element)
+        else:
+            result_list.append([])
+            result_list[-1].append(element)
+
+    return result_list
+
+
 def create_students_list():
     input_total_amount = int(input('Enter total amount of students:'))
-    students_list_ = []  # creating an empty list to add all of them
+    student_list = []  # creating an empty list to add all of them
 
-    while len(students_list_) < input_total_amount:
-        input_student = input(f'Enter the {len(students_list_) + 1}st student:')
-        students_list_.append(input_student)
+    while len(student_list) < input_total_amount:
+        input_student = input(f'Enter the {len(student_list) + 1}st student:')
+        student_list.append(input_student)
 
-    return students_list_
+    return student_list
 
 
 def print_details_from_students_list(students_list_):
+    new_gender_details_list = []
+
     for student in students_list_:
         response_from_gender_details_url = requests.get(f'{GENDER_DETAILS_URL}{student}')
         response_dict_from_gender_details_url = response_from_gender_details_url.json()
+        new_gender_details_list.append(response_dict_from_gender_details_url)
 
-        response_from_nation_url = requests.get(f'{NATION_URL}{student}')
-        response_dict_url_nation = response_from_nation_url.json()
+    divided_list = divide_lists_by_ten(input_list=students_list_)
 
-        print(f'{student} gender is probably - {response_dict_from_gender_details_url["gender"]},'
-              f' with probability {response_dict_from_gender_details_url["probability"]}, '
-              f'nation is {response_dict_url_nation["country"][0]["country_id"]}')
+    for student_chunk in divided_list:
+        url_with_ten_students = NATION_URL + '&name[]='.join(student_chunk)
+        response_from_nation_url = requests.get(url=url_with_ten_students)
+        response_list_url_nation = response_from_nation_url.json()
+
+    for gender_info, country_info in zip(new_gender_details_list, response_list_url_nation):
+        print(f'Name is {gender_info["name"]}')
+        print(f'Gender is {gender_info["gender"]}')
+        if country_info["country"]:
+            print(f'Country is {country_info["country"]}')
+        else:
+            print('country unknown')
 
 
 if __name__ == "__main__":
--- school_journal/test_imports.py
+++ school_journal/test_imports.py
@@ -1,4 +1,7 @@
-from school_journal.shool_journal import print_details_from_students_list, create_students_list
+from school_journal.shool_journal import print_details_from_students_list, create_students_list, divide_lists_by_ten
 
 # qwe = create_students_list()
 # print_details_from_students_list(qwe)
+list_15_people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+res_list = divide_lists_by_ten(input_list=list_15_people)
+print(res_list)
